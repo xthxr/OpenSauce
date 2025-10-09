86a6887e5aa7f8a67269061f54613bc43323f4d0
Debosmita Pal <debosmitapal429@gmail.com>
2025-10-09 22:12:25 +0530

    Adding Wildcard Matching problem

---
+++ Java/dynamic_programming/Wildcard_Matching.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Wildcard_Matching {
+    public static boolean isMatch(String s, String p) {
+        int n = s.length(), m = p.length();
+        boolean[][] dp = new boolean[n + 1][m + 1];
+        dp[0][0] = true;
+        for (int j = 1; j <= m; j++) {
+            if (p.charAt(j - 1) == '*')
+                dp[0][j] = dp[0][j - 1];
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                char pc = p.charAt(j - 1);
+                if (pc == s.charAt(i - 1) || pc == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (pc == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter string s: ");
+        String s = sc.nextLine();
+        System.out.print("Enter pattern p: ");
+        String p = sc.nextLine();
+        if (isMatch(s, p))
+            System.out.println("Match: YES");
+        else
+            System.out.println("Match: NO");
+    }
+}
+
