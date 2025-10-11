b98a8d132603df88196e38cd8432d04111c7384d
AmjustGettingStarted <gamerhmv2050@gmail.com>
2025-10-11 15:51:39 +0530

    feat(java): add triangle pattern

---
+++ Java/patterns/Triangle.java
@@ -0,0 +1,14 @@
+package Java.patterns;
+
+public class Triangle {
+    public static void main(String[] args) {
+        int n = 5; // Number of rows
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
